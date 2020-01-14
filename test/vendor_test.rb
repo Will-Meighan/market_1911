24e73a5c37e7f8dad76e7dbe973bd3cf65a12b81
William Meighan <williamdmeighan@gmail.com>
2020-01-14 09:15:19 -0700

    Setup vendor test file and test for existence

---
+++ test/vendor_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/vendor'
+require_relative '../lib/item'
+
+class VenderTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+end
