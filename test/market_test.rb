3c1ef5ba77977b3011e54e24a2f027b23f079a4a
William Meighan <williamdmeighan@gmail.com>
2020-01-14 09:34:52 -0700

    Setup attributes test

--- test/market_test.rb
+++ test/market_test.rb
@@ -19,5 +19,10 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, @market
   end
 
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
 
 end
